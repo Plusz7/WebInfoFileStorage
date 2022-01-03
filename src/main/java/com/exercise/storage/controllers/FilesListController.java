09bf6b5c96209ddd11f5dc4b14cb19ce3edc9895
dpluszeb <dariusz.pluszczynski@elzab.pl>
2022-01-03 14:45:07 +0100

    minor fixes

--- src/main/java/com/exercise/storage/controllers/FilesListController.java
+++ src/main/java/com/exercise/storage/controllers/FilesListController.java
@@ -1,17 +1,14 @@
 package com.exercise.storage.controllers;
 
-import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 
 public interface FilesListController {
-    @RequestMapping(value = "/fileslist", method = RequestMethod.GET)
+    @GetMapping(value = "/fileslist")
     public ModelAndView filesList();
 
     @GetMapping("/fileslist/{id}")
